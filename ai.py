cd5c398b98d4586109b43d4c3d4e5e4a51c9edc2
Adam SOIMANSOIB <adamsoimansoibpro@gmail.com>
2024-02-05 14:42:30 +0100

    [refactor] Move the Prompt class definition
    Prompt class definition belongs to the file in which Prompt provider is defined.

--- ai.py
+++ ai.py
@@ -1,12 +1,7 @@
 from abc import ABC, abstractmethod
 
+from prompt_provider import Prompt
 from validator import Validator, NotValidException
-
-
-class Prompt:
-    def __init__(self, system_prompt: str, user_prompt: str):
-        self.system_prompt = system_prompt
-        self.user_prompt = user_prompt
 
 
 class AiAbstract(ABC):

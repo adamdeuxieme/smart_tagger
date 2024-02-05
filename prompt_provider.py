d2c6a69b020fe24f70d4d7fffbe7f103bd98e232
Adam SOIMANSOIB <adamsoimansoibpro@gmail.com>
2024-02-05 14:51:46 +0100

    [refactor] Add a Prompt Provider Abstract class
    Prompt Provider class is responsible for prompt construction with given arguments.

--- prompt_provider.py
+++ prompt_provider.py
@@ -1,4 +1,17 @@
+from abc import ABC, abstractmethod
+
+
 class Prompt:
     def __init__(self, system_prompt: str, user_prompt: str):
         self.system_prompt = system_prompt
         self.user_prompt = user_prompt
+
+
+class PromptProvider(ABC):
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def construct_prompt(self, *args) -> Prompt:
+        pass

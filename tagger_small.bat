030a1a752c6629b8527cc812337a8c150f3fa619
Adam SOIMANSOIB <adamsoimansoibpro@gmail.com>
2024-02-06 17:54:10 +0100

    [clean] Delete unnecessary files
    - Delete tagger_big.bat, tagger_recursive.bat and tagger_small.bat
    - Remove tags.md

--- tagger_small.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-python C:\Users\adams\tools\bin\main.py tag -f "%~1" -t C:\Users\adams\tools\bin\tags.md -tn 5 > C:\Users\adams\tools\bin\output.txt
-type C:\Users\adams\tools\bin\output.txt
-pause
-del C:\Users\adams\tools\bin\output.txt

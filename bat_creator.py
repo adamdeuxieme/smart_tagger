ed5825ec43c6c05927f0279ff6ca93c2940bf713
Adam SOIMANSOIB <adamsoimansoibpro@gmail.com>
2024-02-09 12:26:13 +0100

    [patch] Adapt batch creator and fortify cmd line app
    - Bat created script now integrate ai flag
    - Option mainPath is no longer required. Default value is current path
    - Ai can be specified with cmd line options
    - Ai flags now can’t be both true
    - Option outputPath and folderScope option can’t be both true

--- bat_creator.py
+++ bat_creator.py
@@ -1,44 +1,65 @@
+import os
+from enum import Enum
+from typing import Union
+
+
+class AiEnum(Enum):
+    MISTRAL = "--mistralai"
+    CHATGPT = "--chatgpt"
+
+
 class BatCreator:
 
     @staticmethod
-    def create_bat_script(main_folder_path: str,
-                          tags_folder_path: str = None,
-                          tmp_folder_path: str = None,
-                          tag_number: int = None) -> None:
+    def create_bat_script(main_folder_path: Union[str, None] = None,
+                          tags_folder_path: Union[str, None] = None,
+                          tmp_folder_path: Union[str, None] = None,
+                          tag_number: Union[int, None] = None,
+                          model: Union[AiEnum, None] = None) -> None:
         script = BatCreator.construct_bat_script(main_folder_path,
                                                  tags_folder_path,
                                                  tmp_folder_path,
-                                                 tag_number)
+                                                 tag_number,
+                                                 model)
         with open("tagger.bat", 'w') as file:
             file.write(script)
 
     @staticmethod
-    def create_bat_script_folder(main_folder_path: str,
-                          tags_folder_path: str = None,
-                          tag_number: int = None) -> None:
+    def create_bat_script_folder(main_folder_path: Union[str, None] = None,
+                                 tags_folder_path: Union[str, None] = None,
+                                 tag_number: Union[int, None] = None,
+                                 model: Union[AiEnum, None] = None) -> None:
 
         script = BatCreator.construct_bat_script_folder(main_folder_path,
                                                         tags_folder_path,
-                                                        tag_number)
+                                                        tag_number,
+                                                        model)
         with open("tagger_folder.bat", 'w') as file:
             file.write(script)
 
     @staticmethod
-    def construct_bat_script(main_folder_path: str,
-                             tags_folder_path: str = None,
-                             tmp_folder_path: str = None,
-                             tag_number: int = None) -> str:
+    def construct_bat_script(main_folder_path: Union[str, None] = None,
+                             tags_folder_path: Union[str, None] = None,
+                             tmp_folder_path: Union[str, None] = None,
+                             tag_number: Union[int, None] = None,
+                             model: Union[AiEnum, None] = None) -> str:
         if main_folder_path is None:
-            raise ValueError("Main folder path is required.")
+            main_folder_path = os.getcwd()
         tags_folder_path = tags_folder_path if tags_folder_path else main_folder_path
         tmp_folder_path = tmp_folder_path if tmp_folder_path else main_folder_path
         tag_number = tag_number if tag_number else 5
+        if model is None:
+            model = AiEnum.CHATGPT
+        elif not isinstance(model, AiEnum):
+            raise ValueError("The model is not an instance of AiEnum!")
 
         script = ("@echo off\n"
                   f"python {main_folder_path}\\main.py tag "
                   "-f \"%~1\" "
                   f"-t {tags_folder_path}\\tags.md "
-                  f"-tn {tag_number} > "
+                  f"-tn {tag_number} "
+                  f"{model.value} "
+                  "> "
                   f"{tmp_folder_path}\\output.txt\n"
                   f"type {tmp_folder_path}\\output.txt\n"
                   "pause\n"
@@ -48,12 +69,17 @@ class BatCreator:
     @staticmethod
     def construct_bat_script_folder(main_folder_path: str,
                                     tags_folder_path: str = None,
-                                    tag_number: int = None) -> str:
+                                    tag_number: int = None,
+                                    model: Union[AiEnum, None] = None) -> str:
 
         if main_folder_path is None:
-            raise ValueError("Main folder path is required.")
+            main_folder_path = os.getcwd()
         tags_folder_path = tags_folder_path if tags_folder_path else main_folder_path
         tag_number = tag_number if tag_number else 4
+        if model is None:
+            model = AiEnum.CHATGPT
+        elif not isinstance(model, AiEnum):
+            raise ValueError("The model is not an instance of AiEnum!")
 
         script = ("@echo off\n"
                   "setlocal enabledelayedexpansion\n"
@@ -76,7 +102,9 @@ class BatCreator:
                   f"python {main_folder_path}\\main.py tag "
                   f"-f \"%%i\" "
                   f"-t {tags_folder_path}\\tags.md "
-                  f"-tn {tag_number} > nul\n        "
+                  f"-tn {tag_number} "
+                  f"{model.value} "
+                  "> nul\n        "
                   f"set /a \"count+=1\"\n    "
                   f")\n    "
                   f"echo Processed !count! out of !total! files.\n"
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 
 from ai import MistralAi, MistralAiModel, ChatGptAi, ChatGPTModel
-from bat_creator import BatCreator
+from bat_creator import BatCreator, AiEnum
 from prompt_provider import Prompt
 from tagger import Tagger, TaggerValidator
 
@@ -16,14 +16,20 @@ def main():
     tag_parser.add_argument('--tagNumber', '-tn', type=int, required=False)
     tag_parser.add_argument('--temperature', '-temp', type=float, required=False)
     tag_parser.add_argument('--model', '-m', type=MistralAiModel, required=False)
-    tag_parser.add_argument('--mistralai', action="store_true")
-    tag_parser.add_argument('--chatgpt', action="store_true")
+    ai_group = tag_parser.add_mutually_exclusive_group()
+    ai_group.add_argument('--mistralai', action="store_true")
+    ai_group.add_argument('--chatgpt', action="store_true")
+
     bat_parser = subparser.add_parser("bat")
-    bat_parser.add_argument('--mainPath', '-f', type=str, required=True)
+    folder_scope_output_path_exclusive_grp = bat_parser.add_mutually_exclusive_group()
+    ai_group = bat_parser.add_mutually_exclusive_group()
+    bat_parser.add_argument('--mainPath', '-f', type=str, required=False)
     bat_parser.add_argument('--tagPath', '-t', type=str, required=False)
-    bat_parser.add_argument('--outputPath', '-o', type=str, required=False)
+    folder_scope_output_path_exclusive_grp.add_argument('--outputPath', '-o', type=str, required=False)
     bat_parser.add_argument('--tagNumber', '-tn', type=int, required=False)
-    bat_parser.add_argument('--folderScope', action="store_true")
+    ai_group.add_argument('--mistralai', action="store_true")
+    ai_group.add_argument('--chatgpt', action="store_true")
+    folder_scope_output_path_exclusive_grp.add_argument('--folderScope', action="store_true")
     args = parser.parse_args()
 
     if args.command == 'tag':
@@ -49,10 +55,14 @@ def main():
             print("Artificial Intelligence to use not specified. Please specify it.")
 
     elif args.command == 'bat':
+        model = None
+        if args.mistralai or args.chatgpt:
+            model = AiEnum.CHATGPT if args.chatgpt else AiEnum.MISTRAL
+
         if args.folderScope:
-            BatCreator.create_bat_script_folder(args.mainPath, args.tagPath, args.tagNumber)
+            BatCreator.create_bat_script_folder(args.mainPath, args.tagPath, args.tagNumber, model)
         else:
-            BatCreator.create_bat_script(args.mainPath, args.tagPath, args.outputPath, args.tagNumber)
+            BatCreator.create_bat_script(args.mainPath, args.tagPath, args.outputPath, args.tagNumber, model)
 
 
 if __name__ == '__main__':
